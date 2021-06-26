e10a75b5129a5b3857f29012b3dbb3a2cd57adf3
Kai Chan <chankw.jh@gmail.com>
2021-06-26 22:57:05 +0100

    leetcode 6. ZigZag Conversion

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,5 +1,33 @@
-public class Solution {
+class Solution {
     public String solution() {
         return "hello";
     }
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s == null || s.length() == 0) {
+            return s;
+        }
+        StringBuilder[] rowBuilders = new StringBuilder[numRows];
+        int row = 0;
+        int factor = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (rowBuilders[row] == null) {
+                rowBuilders[row] = new StringBuilder();
+            }
+            final StringBuilder sb = rowBuilders[row];
+            sb.append(s.charAt(i));
+            if (row == numRows - 1) {
+                factor *= -1;
+            } else if (row == 0) {
+                factor = +1;
+            }
+            row += factor;
+        }
+        final StringBuilder out = new StringBuilder();
+        for (StringBuilder rowBuilder : rowBuilders) {
+            if (rowBuilder != null) out.append(rowBuilder);
+        }
+        return out.toString();
+    }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -8,4 +8,11 @@ public class SolutionTest {
     void t1() {
         assertThat(new Solution().solution()).isEqualTo("123");
     }
+
+    @Test
+    void convert() {
+        assertThat(new Solution().convert("PAYPALISHIRING", 3)).isEqualTo("PAHNAPLSIIGYIR");
+        assertThat(new Solution().convert("A", 2)).isEqualTo("A");
+        assertThat(new Solution().convert("AB", 1)).isEqualTo("AB");
+    }
 }
